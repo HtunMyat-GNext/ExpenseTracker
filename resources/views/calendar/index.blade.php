407d3735200c5965bcaf3114d733252bded6149f
HtunMyat-GNext <gnext.htunhtunmyat@gmail.com>
2024-06-11 14:56:13 +0630

    add calendar event

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -8,7 +8,7 @@
                 {{ __('Calendar') }}
             </h2>
             <div class="text-right ml-auto">
-                <a href="{{ route('income.create') }}"
+                <a href="{{ route('events.index') }}"
                     class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">See
                     Calendar Events</a>
             </div>
@@ -23,22 +23,11 @@
                         <p class="text-center p-4">
                             <strong>Your event lists</strong>
                         </p>
-
-                        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event m-2'>
-                            <div class='fc-event-main p-2 text-center p-2 text-center'>Meeting with Team</div>
-                        </div>
-                        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event m-2'>
-                            <div class='fc-event-main p-2 text-center'>App Release</div>
-                        </div>
-                        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event m-2'>
-                            <div class='fc-event-main p-2 text-center'>To do Task</div>
-                        </div>
-                        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event m-2'>
-                            <div class='fc-event-main p-2 text-center'>Client Meeting</div>
-                        </div>
-                        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event m-2'>
-                            <div class='fc-event-main p-2 text-center'>My Birthday</div>
-                        </div>
+                        @foreach ($events as $event)
+                            <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event m-2'>
+                                <div class='fc-event-main p-2 text-center p-2 text-center'>{{ $event->title }}</div>
+                            </div>
+                        @endforeach
                     </div>
 
                     <div id='calendar-container' class="col-span-4">
@@ -112,6 +101,7 @@
                     },
                     editable: true,
                     droppable: true, // this allows things to be dropped onto the calendar
+                    eventColor: 'green'
                 });
 
                 calendar.render();
