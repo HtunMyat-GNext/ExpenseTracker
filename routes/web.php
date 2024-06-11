e0e09af8b04400b3a3970f9ef33853f529891865
HtunMyat-GNext <gnext.htunhtunmyat@gmail.com>
2024-06-11 13:53:40 +0630

    solve route conflict

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('categories', CategoryController::class);
-    Route::get('/income/{filter?}', [IncomeController::class, 'index'])->name('income.index');
     Route::resource('income', IncomeController::class)->except('show','index');
+    Route::get('/income/{filter?}', [IncomeController::class, 'index'])->name('income.index');
     Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar');
 
     Route::resource('expenses', ExpenseController::class);
